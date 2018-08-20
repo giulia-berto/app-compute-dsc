525c877f22690354d6cb93b7670ae125b075f074
Giulia Berto <gberto@hm014.karst.uits.iu.edu>
2018-08-20 05:56:24 -0400

    from 4 to 8 optional tracts

--- afqConverter2.m
+++ afqConverter2.m
@@ -19,7 +19,7 @@ load(fullfile(config.segmentation1));
 fid=fopen('tract_name_list.txt', 'w');
 
 if (config.tract1 > 0)
-    for tract = [config.tract1, config.tract2, config.tract3, config.tract4]
+    for tract = [config.tract1, config.tract2, config.tract3, config.tract4, config.tract5, config.tract6, config.tract7, config.tract8]
         if (tract > 0)
             tract_name=strrep(fg_classified(tract).name,' ','_');
             write_fg_to_trk_shift(fg_classified(tract),ref_src_sub1,sprintf('%s_tract_%s.trk',tract_name,run));
@@ -40,7 +40,7 @@ fclose(fid);
 load(fullfile(config.segmentation2));
 
 if (config.tract1 > 0)
-    for tract = [config.tract1, config.tract2, config.tract3, config.tract4]
+    for tract = [config.tract1, config.tract2, config.tract3, config.tract4, config.tract5, config.tract6, config.tract7, config.tract8]
         if (tract > 0)
             tract_name=strrep(fg_classified(tract).name,' ','_');
             write_fg_to_trk_shift(fg_classified(tract),ref_src_sub2,sprintf('%s_tract.trk',tract_name));
