fd5789679813b78cb82dc401c9fa4cd14f25d656
Giulia Berto <gberto@hm014.karst.uits.iu.edu>
2018-08-20 06:16:48 -0400

    from 4 to 8 optional tracts

--- afqConverter2.m
+++ afqConverter2.m
@@ -18,21 +18,11 @@ disp('Converting afq to .trk');
 load(fullfile(config.segmentation1));
 fid=fopen('tract_name_list.txt', 'w');
 
-if (config.tract1 > 0)
-    for tract = [config.tract1, config.tract2, config.tract3, config.tract4, config.tract5, config.tract6, config.tract7, config.tract8]
-        if (tract > 0)
-            tract_name=strrep(fg_classified(tract).name,' ','_');
-            write_fg_to_trk_shift(fg_classified(tract),ref_src_sub1,sprintf('%s_tract_%s.trk',tract_name,run));
-            fprintf(fid, [tract_name, '\n']);
-        end    
-    end    
-else
-    for tract=1:length(fg_classified)
-        tract_name=strrep(fg_classified(tract).name,' ','_');
-        write_fg_to_trk_shift(fg_classified(tract),ref_src_sub1,sprintf('%s_tract_%s.trk',tract_name,run));
-        fprintf(fid, [tract_name, '\n']);  
-    end 
-end
+for tract=1:length(fg_classified)
+    tract_name=strrep(fg_classified(tract).name,' ','_');
+    write_fg_to_trk_shift(fg_classified(tract),ref_src_sub1,sprintf('%s_tract_%s.trk',tract_name,run));
+    fprintf(fid, [tract_name, '\n']);  
+end 
 
 fclose(fid);
 
